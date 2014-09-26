60cef112a89be25f685b3e77129b0a14fd579f5b
namba1 <namba1@oc7735527180>
2014-09-26 15:04:56 +0900

    up

--- helloWorld.rb
+++ helloWorld.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'haml' # template engine
 require 'json'
+require 'mustache/sinatra'
+require 'oauth2'
 
 # control part of MVC
 # an HTTP method paired with a URL-matching pattern
@@ -18,19 +20,11 @@ get '/' do
     end
   end
   
-  #There are many useful environment variables available in process.env,
-  #please refer to the following document for detailed description:
-  #http://docs.cloudfoundry.com/docs/using/deploying-apps/environment-variable.html
-  
-  #VCAP_APPLICATION contains useful information about a deployed application.
   appInfo = @env["VCAP_APPLICATION"]
-
-  #VCAP_SERVICES contains all the credentials of services bound to
-  #this application. For details of its content, please refer to
-  #the document or sample of each service.
   services = @env["VCAP_SERVICES"]
   #TODO: Get service credentials and communicate with bluemix services.
 
   # render template
-  haml :hi
+  #haml :hi
+  mustache :login
 end
