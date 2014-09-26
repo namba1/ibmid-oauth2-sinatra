144f4d6e999a8522b48fad3db96b77a721d378a3
namba1 <namba1@oc7735527180>
2014-09-26 18:05:27 +0900

    up

--- ibmidTest.rb
+++ ibmidTest.rb
@@ -8,7 +8,8 @@ class App < Sinatra::Base
 	register Mustache::Sinatra
 	require './views/layout'
 	
-	include Rack::Utils; alias_method :h, :escape_html
+	include Rack::Utils
+	alias_method :h, :escape_html
 	
 	set :mustache, {
 		:views     => './views',
@@ -17,24 +18,39 @@ class App < Sinatra::Base
 	############################################
 	
 	get '/' do
-=begin
-	  # page variable
 	  @version = RUBY_VERSION
 	  @os = RUBY_PLATFORM
-	  @env = {}
-	  ENV.each do |key, value|
-	    begin
-	      hash = JSON.parse(value)
-	      @env[key] = hash
-	    rescue
-	      @env[key] = value
-	    end
-	  end
-	  
-	  @appInfo = @env["VCAP_APPLICATION"]
-	  @services = @env["VCAP_SERVICES"]
-=end
+
+	  @appInfo = ENV["VCAP_APPLICATION"]
+	  @services = ENV["VCAP_SERVICES"]
 
 	  mustache :login
 	end
 end 
+
+=begin
+    options = {
+        authorizationURL: 'https://idaas.ng.bluemix.net/sps/oauth20sp/oauth20/authorize',
+        tokenURL: 'https://idaas.ng.bluemix.net/sps/oauth20sp/oauth20/token',
+        profileURL: 'https://idaas.ng.bluemix.net/idaas/resources/profile.jsp',
+        logoutURL: 'https://www-304.ibm.com/pkmslogout?page=',
+        policy: 'http://www.ibm.com/idaas/authnpolicy/reauth',
+        scope: ['profile']
+    };
+    {
+			"single.sign.on":[
+				{
+					"name":"Single Sign On -kr",
+					"label":"single.sign.on",
+					"tags":["ibm_created","security"],
+					"plan":"standard",		
+					"credentials":{
+						"profile_resource":"https://idaas.ng.bluemix.net/idaas/resources/profile.jsp",
+						"tokeninfo_resource":"https://idaas.ng.bluemix.net/idaas/resources/tokeninfo.jsp",
+						"token_url":"https://idaas.ng.bluemix.net/sps/oauth20sp/oauth20/token",
+						"authorize_url":"https://idaas.ng.bluemix.net/sps/oauth20sp/oauth20/authorize"
+					}
+				}
+			]
+		}	
+=end
--- views/login.rb
+++ views/login.rb
@@ -2,15 +2,21 @@ class App < Sinatra::Base
 	module Views
 		class Login < Mustache
 			
-			def doit
-				"This is a test"
+			def info_msg
+				"Ruby Version: #{@version}  Platform: #{@os}"
 			end
 			
+			def appInfo
+				@appInfo
+			end
+		  def services
+	  		@services
+	  	end
+	  			
 			def version
 				"0.01"
 			end	
-				
-		end
+		end		
 	end
 
 end
